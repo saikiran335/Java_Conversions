dd7281b3cb4d9acb400f6e228136ef86f5645507
Sai_kiran <31896636+saikiran335@users.noreply.github.com>
2020-11-04 20:37:53 +0530

    Add files via upload

---
+++ stringtolong.java
@@ -0,0 +1,11 @@
+public class stringtolong
+{
+public static void main(String[] args)
+{
+String s="1000000000";
+long l=Long.parseLong(s);
+System.out.println(l);
+System.out.println(s+30);
+System.out.println(l+30);
+}
+}
