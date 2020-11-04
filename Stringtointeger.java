91c4153c02a5471a541f51243383eb4da6697588
Sai_kiran <31896636+saikiran335@users.noreply.github.com>
2020-11-04 20:36:26 +0530

    Add files via upload

---
+++ Stringtointeger.java
@@ -0,0 +1,11 @@
+public class stringtointeger
+{
+public static void main(String[] args)
+{
+String s="100";
+int i=Integer.parseInt(s);
+System.out.println(i);
+System.out.println(s+30);
+System.out.println(i+30);
+}
+}
