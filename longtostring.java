b49bee9e1aa13beee4c26335c21492eca7c55077
Sai_kiran <31896636+saikiran335@users.noreply.github.com>
2020-11-04 21:34:22 +0530

    Add files via upload

---
+++ longtostring.java
@@ -0,0 +1,11 @@
+public class longtostring
+{
+public static void main(String[] args)
+{
+Long l=9110766827L;
+String s=String.valueOf(l);
+System.out.println(s);
+System.out.println(l+30);
+System.out.println(s+30);
+}
+}
