52e73bf4afdb27f872bb1bf8f3a825aafb2b2001
Sai_kiran <31896636+saikiran335@users.noreply.github.com>
2020-11-04 20:37:10 +0530

    Add files via upload

---
+++ integertostring.java
@@ -0,0 +1,11 @@
+public class integertostring
+{
+public static void main(String[] args)
+{
+int i=100;
+String s=String.valueOf(i);
+System.out.println(s);
+System.out.println(i+30);
+System.out.println(s+30);
+}
+}
