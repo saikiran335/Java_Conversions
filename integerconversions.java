b28142e9bf3091531dc37aae59d0fdc4902f728b
Sai_kiran <31896636+saikiran335@users.noreply.github.com>
2020-11-05 06:31:38 +0530

    Add files via upload

---
+++ integerconversions.java
@@ -0,0 +1,18 @@
+public class integerconversions
+{
+public static void main(String[] args)
+{
+int i=100;
+String s=String.valueOf(i);
+float f=i;
+long l=i;
+double d=i;
+char c=(char)i;
+System.out.println(i);
+System.out.println(s);
+System.out.println(f);
+System.out.println(l);
+System.out.println(d);
+System.out.println(c);
+}
+}
